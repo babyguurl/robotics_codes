a64e90a19925acbb0304fce9ed5dd37ccf7fbdb1
babyguurl <165135723+babyguurl@users.noreply.github.com>
2024-12-16 12:18:51 +0300

    Creat blink_different_LEDs file

---
+++ blink_differnt_color_LEDs.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(4,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(12,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(12,HIGH);
+  delay(1000);
+  digitalWrite(12,HIGH);
+  delay(1000);
+}
